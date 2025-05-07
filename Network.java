842e3d43dc8f5306ef629b6226144ae53f92cd36
sarahqii <152104311+sarahqii@users.noreply.github.com>
2025-05-07 15:04:37 -0500

    Update Network.java

--- Network.java
+++ Network.java
@@ -15,23 +15,27 @@ public class Network {
     private int agentNum;
     private int activeAgentNum;
     private int activeAgentNumPrevious;
+    private int[] curModes;
+    private int[] preModes;
     private int gridRow;
-    private int b;
+    private double b;
     private double m;
-    private int T;
+    private double T;
     
-    public Network(int agentNum, int b, double m, int T){
+    public Network(int agentNum, double b, double m, double T){
         this.agents = new Agent[agentNum + 1];
         for(int i = 1; i <= agentNum; i++){
             agents[i] = new Agent(i, 0, agentNum, b, m);
         }
         this.agentNum = agentNum;
         activeAgentNum = agentNum;
-        activeAgentNumPrevious = agentNum;
+        activeAgentNumPrevious = 0;
         gridRow = 0;
         this.b = b;
         this.m = m;
         this.T = T;
+        curModes = new int[agentNum + 1];
+        preModes = new int[agentNum + 1];
     }
     
     public void initialDefector(int h){
@@ -46,11 +50,17 @@ public class Network {
     public void playOneRound(){
         for (int i = 1; i <= agentNum; i++) {
         Agent agent = agents[i];
+        if(agent.getState() == 0){
+            continue;
+        }
         agent.refreshUnplayed();
-        agent.addScore(0); 
+        agent.setScore(0); 
         }
         for (int i = 1; i <= agentNum; i++) {
             Agent agent = agents[i];
+            if(agent.getState() == 0){
+            continue;
+            }
             agent.playAll(this);
         }
     }
@@ -62,11 +72,64 @@ public class Network {
         }
     }
     
+    public int findMaxK(){
+        double max = -1;
+        int maxIndex = -1;
+        for(int i = 1; i <= agentNum; i++){
+            Agent agent = agents[i];
+            if(agent.getK() > max){
+                max = agent.getK();
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+    
+    public int getCNum(){
+        int counter = 0;
+        for(int i = 1; i <= agentNum; i++){
+            Agent agent = agents[i];
+            if(agent.getMode() == 0){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    
+    public int getDNum(){
+        int counter = 0;
+        for(int i = 1; i <= agentNum; i++){
+            Agent agent = agents[i];
+            if(agent.getMode() == 1){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    
+    public int findMinK(){
+        double min = 9999999;
+        int minIndex = -1;
+        for(int i = 1; i <= agentNum; i++){
+            Agent agent = agents[i];
+            if(agent.getK() < min){
+                min = agent.getK();
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+    
+    
     public void eliminate(){
         activeAgentNumPrevious = activeAgentNum;
         for (int i = 1; i <= agentNum; i++) {
             Agent agent = agents[i];
-            if (agent.getScore() < T/agent.getPairNum()) {
+            if(agent.getState() == 0){
+                continue;
+            }
+            if (agent.getScore() < T/agent.getK()) {
+                agent.setState(0);
                 agent.cleanPaired();
                 for(int j = 1; j <= agentNum; j++){
                     if(j == i){
@@ -80,12 +143,17 @@ public class Network {
     }
     
     public boolean checkEnd(){
+        //System.out.println("Now active Agent Num is: "+activeAgentNum);
+        //System.out.println("activeAgentNum: " + activeAgentNum);
+        //System.out.println("activeAgentNumPrevious: " + activeAgentNumPrevious);
         if(activeAgentNum == 0){
             return true;
         }
-        else if(activeAgentNum == activeAgentNumPrevious){
+        
+        else if((activeAgentNum == activeAgentNumPrevious) && checkStable()){
             return true;
         }
+
         else{
             return false;
         }
@@ -115,10 +183,52 @@ public class Network {
     public void printResult(){
         for(int i = 1; i <= agentNum; i++){
             Agent agent = agents[i];
-            if(agent.getPairNum() != 0){
+            int pairNum = agent.getPairNum();
+            if(pairNum != 0){
                 System.out.println(agent.getIndex());
             }
         }
     
+    }
+    
+    public void collectMode(){
+        for (int i = 1; i <= agentNum; i++) {
+        curModes[i] = agents[i].getMode();
+        }
+    }
+    
+    public void copyToPreMode(){
+        System.arraycopy(curModes, 0, preModes, 0, agentNum+1);
+    }
+    
+    public boolean checkStable(){
+        return Arrays.equals(preModes, curModes);
+    }
+    
+    public void printState(){
+        for(int i = 1; i <= agentNum; i++){
+            Agent agent = agents[i];
+            PriorityQueue<Integer> pairs = agent.getPairs();
+            System.out.print(i);
+            if(agent.getMode() == 0){
+                System.out.print("(C)");
+            }
+            else{
+                System.out.print("(D)");
+            }
+            System.out.print("(K: "+agent.getK()+", Score: " + agent.getScore() +"): ");
+            for(int pairedIndex: pairs){
+                System.out.print(pairedIndex+" ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    public int getActiveAgentNum(){
+        return activeAgentNum;
+    }
+    
+    public int getAgentNum(){
+        return agentNum;
     }
 }
