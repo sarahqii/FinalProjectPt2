88632501d1c9eb7cb5584c2c3e9f686993465f03
sarahqii <152104311+sarahqii@users.noreply.github.com>
2025-05-07 15:04:07 -0500

    Update FinalProjectPartII.java

--- FinalProjectPartII.java
+++ FinalProjectPartII.java
@@ -22,23 +22,22 @@ import java.io.*;
  */
 public class FinalProjectPartII {
     public static void main(String[] args) throws Exception {
-        String path = "output.txt";
+        String path = "option6.txt";
         BufferedReader bfro = new BufferedReader (new FileReader(path));
         String st;
         int N = 0;
         bfro = new BufferedReader (new FileReader(path));
         while((st = bfro.readLine()) != null){
-          //  System.out.println(st);
             String[] arr = st.split(" ");
             int i1 = Integer.parseInt(arr[0]);
             if (i1 > N){N = i1;}
             int i2 = Integer.parseInt(arr[1]);
             if (i2 > N){N = i2;}
         }
-        //System.out.println("\n\n\n");
-        double m = 0.6;
-        int T = 1;
-        Network network = new Network(N, 1, m, T);
+        double m = 0.3;
+        double T = 1;
+        double b = 2.0;
+        Network network = new Network(N, b, m, T);
         bfro = new BufferedReader (new FileReader(path));
         while((st = bfro.readLine()) != null){
             String[] arr = st.split(" ");
@@ -47,22 +46,48 @@ public class FinalProjectPartII {
             Agent agent = network.getAgent(i1);
             agent.pair(i2, network.getAgent(i2));
         }
-        network.printAllPairs();
-        System.out.println("\n\n\nGame Start:");
+        
+        int h = 700;
+        network.initialDefector(h);
+        //network.printState();
+        network.collectMode();
+        PrintWriter writer = new PrintWriter(new FileWriter("output.txt"));
+        System.out.println("Game Start:");
+        int maxIndex = network.findMaxK();
+        int minIndex = network.findMinK();
+        
+        System.out.println("Agent " + maxIndex + " has the largest k with k = " + network.getAgent(maxIndex).getK());
+        System.out.println("Agent " + minIndex + " has the smallest k with k = " + network.getAgent(minIndex).getK());
+        System.out.println("Parameters:");
+        System.out.println("b = " + b + ", h = " + h + ", T = " + T + ", m = " + m);
+        
+        writer.println("Agent " + maxIndex + " has the largest k with k = " + network.getAgent(maxIndex).getK());
+        writer.println("Agent " + minIndex + " has the smallest k with k = " + network.getAgent(minIndex).getK());
+        writer.println("Parameters:");
+        writer.println("b = " + b + ", h = " + h + ", T = " + T + ", m = " + m);
+        
+
         
         int counter = 0;
-        int h = 3;
-        network.initialDefector(h);
         while(!network.checkEnd()){
             network.playOneRound();
             network.AllLearnStrategy();
+            network.copyToPreMode();
+            network.collectMode();
             network.eliminate();
             counter++;
+            System.out.println("Game Round " + counter);
+            writer.println("Game Round " + counter);
+            double activeAgentNum = (double)network.getActiveAgentNum();
+            double agentNum = (double)network.getAgentNum();
+            double eliminatedAgentNum = agentNum - activeAgentNum;
+            System.out.println("There are " + network.getCNum() + " Coorperators");
+            System.out.println("There are " + network.getDNum() + " Defectors");
+            System.out.println("The percentage of eliminated agents is " + (eliminatedAgentNum / agentNum) * 100 + "%");
+            writer.println("The percentage of eliminated agents is " + (eliminatedAgentNum / agentNum) * 100 + "%");
+            //network.printState();
         }
-        network.printAllPairs();
-        System.out.println("\n\n\n");
-        network.printResult();
         
-        
+        writer.close();
     }
 }
